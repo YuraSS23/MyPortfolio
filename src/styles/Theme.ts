13a237fa49efe354b08048f7e46f139f2ed51fed
YuraSS23 <nikitsenko_yura@mail.ru>
2023-08-03 15:54:32 +0300

    Mobile header

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -8,5 +8,9 @@ colors: {
 
     borderColor: "#4A4A4A",
     placeholderColor: "#495057"
-}
+},
+    media: {
+        tablet: "screen and (max-width: 768px)",
+        mobile: "screen and (max-width: 576px)",
+    }
 }

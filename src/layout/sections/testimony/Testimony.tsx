cbfe9113391267311f59a4f5b8f8a1cb2b2b6617
YuraSS23 <nikitsenko_yura@mail.ru>
2023-08-04 15:57:20 +0300

    Mobile testimony

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -5,25 +5,28 @@ import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {IconWrapper} from "../skills/skill/Skill";
+import {Container} from "../../../components/Container";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
-            <SectionTitle>Testimony</SectionTitle>
-            <FlexWrapper direction={"column"} align={"center"}>
-                <IconWrapper>
-                    <Icon iconId={"quote"}/>
-                </IconWrapper>
-                <Slider/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>Testimony</SectionTitle>
+                <FlexWrapper direction={"column"} align={"center"}>
+                    <IconWrapper>
+                        <Icon iconId={"quote"}/>
+                    </IconWrapper>
+                    <Slider/>
+                </FlexWrapper>
+            </Container>
         </StyledTestimony>
     );
 };
 
 const StyledTestimony = styled.section`
   min-height: 50vh;
-  
+
   ${IconWrapper} {
-    margin: 40px 0 72px; 
+    margin: 40px 0 72px;
   }
 `

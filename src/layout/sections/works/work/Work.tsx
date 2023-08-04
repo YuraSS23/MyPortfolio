c6f54c57b58b901f7eed79470995b8d3eb3b06b5
YuraSS23 <nikitsenko_yura@mail.ru>
2023-08-04 16:06:13 +0300

    Mobile works

--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -30,10 +30,10 @@ export const Work = (props: WorkPropsType) => {
 };
 
 const StyledWork = styled.div`
-  max-width: 540px;
-  width: 100%;
+  width: 330px;
   background-color: ${theme.colors.secondaryBg};
-  
+  flex-grow: 1;
+
   ${Link} {
     padding: 10px 0;
 
@@ -41,40 +41,57 @@ const StyledWork = styled.div`
       margin-left: 20px;
     }
   }
+
+  @media ${theme.media.desktop} {
+    max-width: 540px;
+  }
 `
 
 const ImageWrapper = styled.div`
-  position: relative;
-  
-  &:hover{
-    &::before {
-      content: "";
-      position: absolute;
-      left: 0;
-      right: 0;
-      top: 0;
-      bottom: 0;
-      background: rgba(0, 0, 0, 0.30);
-      backdrop-filter: blur(4px);
-      }
+position: relative;
 
-    ${Button} {
-      opacity: 1;
-    }
-  }
-  
-  ${Button} {
+
+
+${Button} {
     opacity: 0;
     position: absolute;
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
     
-    &::before {
-      width: 100%;
-      height: 100%;
+&::before {
+        width: 100%;
+        height: 100%;
     }
-  }
+}
+&::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.30);
+    backdrop-filter: blur(4px);
+    opacity: 0;
+}
+
+&:hover {
+&::before{
+        opacity: 1;
+    }
+    ${Button} {
+        opacity: 1;
+    }
+}
+@media ${theme.media.tablet} {
+&::before{
+    opacity: 1;
+}
+${Button} {
+    opacity: 1;
+}
+}
 `
 
 const Image = styled.img`
--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -12,5 +12,6 @@ colors: {
     media: {
         tablet: "screen and (max-width: 768px)",
         mobile: "screen and (max-width: 576px)",
+        desktop: "screen and (min-width: 1140px)",
     }
 }

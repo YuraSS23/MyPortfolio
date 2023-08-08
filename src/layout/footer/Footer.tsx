762ce6834ec4d46be4b9520b78b3278208504511
YuraSS23 <nikitsenko_yura@mail.ru>
2023-08-08 14:27:51 +0300

    Mobile footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
 import {theme} from "../../styles/Theme";
+import {font} from "../../styles/Common";
 
 export const Footer = () => {
     return (
@@ -48,10 +49,11 @@ const StyledFooter = styled.footer`
 `
 
 const Name = styled.span`
-  font-family: "Josefin Sans", sans-serif;
-  font-weight: 700;
-  font-size: 22px;
-  letter-spacing: 3px;
+  ${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 22, Fmin: 16})};
+/*  font-family: "Josefin Sans", sans-serif;
+font-weight: 700;
+font-size: 22px; */
+letter-spacing: 3px;
 `
 
 const SocialList = styled.ul`

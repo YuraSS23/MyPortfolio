7a94e4b7b8d6cccdcea87e83571f875a39c1a8fb
YuraSS23 <nikitsenko_yura@mail.ru>
2023-08-14 11:01:34 +0300

    Refactoring Main, Skills

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,7 +4,7 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
-import {IconWrapper} from "../skills/skill/Skill";
+import {S} from "../skills/Skills_styles"
 import {Container} from "../../../components/Container";
 
 export const Testimony = () => {
@@ -13,9 +13,9 @@ export const Testimony = () => {
             <Container>
                 <SectionTitle>Testimony</SectionTitle>
                 <FlexWrapper direction={"column"} align={"center"}>
-                    <IconWrapper>
+                    <S.IconWrapper>
                         <Icon iconId={"quote"}/>
-                    </IconWrapper>
+                    </S.IconWrapper>
                     <Slider/>
                 </FlexWrapper>
             </Container>
@@ -26,7 +26,7 @@ export const Testimony = () => {
 const StyledTestimony = styled.section`
   min-height: 50vh;
 
-  ${IconWrapper} {
+  ${S.IconWrapper} {
     margin: 28px 0 72px;
   }
 `

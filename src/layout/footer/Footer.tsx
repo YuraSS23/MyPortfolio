67b0fe08de1975f33a91a53e77da17111d42d06c
YuraSS23 <nikitsenko_yura@mail.ru>
2023-08-15 14:42:12 +0300

    Refactoring Slogan, Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,90 +1,41 @@
 import React from 'react';
-import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
-import {theme} from "../../styles/Theme";
-import {font} from "../../styles/Common";
+import {S} from "./Footer_Styles";
 
-export const Footer = () => {
+const socialItemsData = [
+    {
+        iconId: "instagram"
+    },
+    {
+        iconId: "telegram"
+    },
+    {
+        iconId: "vk"
+    },
+    {
+        iconId: "linkedin"
+    }
+]
+
+export const Footer: React.FC = () => {
     return (
-        <StyledFooter>
+        <S.Footer>
             <FlexWrapper direction={"column"} align={"center"}>
-                <Name>Yury</Name>
-                <SocialList>
-
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon height={"21px"} width={"21px"} viewBox={"0 0 21 21"} iconId={"instagram"}/>
-                        </SocialLink>
-                    </SocialItem>
-
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon height={"21px"} width={"21px"} viewBox={"0 0 21 21"} iconId={"telegram"}/>
-                        </SocialLink>
-                    </SocialItem>
-
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon height={"21px"} width={"21px"} viewBox={"0 0 21 21"} iconId={"vk"}/>
-                        </SocialLink>
-                    </SocialItem>
-
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon height={"21px"} width={"21px"} viewBox={"0 0 21 21"} iconId={"linkedin"}/>
-                        </SocialLink>
-                    </SocialItem>
-
-                </SocialList>
-                <Copyright>© 2023 Yury Nikitenko, All Rights Reserved.</Copyright>
+                <S.Name>Yury</S.Name>
+                <S.SocialList>
+                    {socialItemsData.map((s, index)=>{
+                        return (
+                            <S.SocialItem key={index}>
+                                <S.SocialLink>
+                                    <Icon height={"21px"} width={"21px"} viewBox={"0 0 21 21"} iconId={s.iconId}/>
+                                </S.SocialLink>
+                            </S.SocialItem>
+                        )
+                    })}
+                </S.SocialList>
+                <S.Copyright>© 2023 Yury Nikitenko, All Rights Reserved.</S.Copyright>
             </FlexWrapper>
-        </StyledFooter>
+        </S.Footer>
     );
 };
-
-const StyledFooter = styled.footer`
-  background-color: ${theme.colors.primaryBg};
-  padding: 40px 0;
-`
-
-const Name = styled.span`
-  ${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 22, Fmin: 16})};
-/*  font-family: "Josefin Sans", sans-serif;
-font-weight: 700;
-font-size: 22px; */
-letter-spacing: 3px;
-`
-
-const SocialList = styled.ul`
-  display: flex;
-  gap: 20px;
-  margin: 30px 0;
-`
-
-const SocialItem = styled.li`
-`
-
-const SocialLink = styled.a`
-  border-radius: 50%;
-  background-color: rgba(255, 255, 255, 0.10);
-  width: 35px;
-  height: 35px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: ${theme.colors.accent};
-  
-  &:hover {
-    color: ${theme.colors.primaryBg};
-    transform: translateY(-4px);
-    background-color: ${theme.colors.accent};
-  }
-`
-
-const Copyright = styled.small`
-  font-weight: 400;
-  font-size: 12px;
-  text-align: center;
-  opacity: 0.5;
-`
--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
-import {Slider} from "../../../components/Slider";
+import {Slider} from "../../../components/slider/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {S} from "../skills/Skills_styles"
 import {Container} from "../../../components/Container";
 
-export const Testimony = () => {
+export const Testimony: React.FC = () => {
     return (
         <StyledTestimony>
             <Container>
